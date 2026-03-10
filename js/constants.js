47ca7ab8cff795ce1a5b014565debb39865b1b34
NicoLBH <nicolas.lebihan@socotec.com>
2026-03-10 18:35:45 +0100

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,7 @@
+export const PROJECT_TABS = [
+  { id: "dashboard", label: "Tableau de bord" },
+  { id: "identity", label: "Fiche d'identité" },
+  { id: "situations", label: "Situations" },
+  { id: "intervenants", label: "Intervenants" },
+  { id: "documents", label: "Documents" }
+];
