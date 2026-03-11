5c7f2b751eb0a4886d702ae9ea616b8f44864fb3
NicoLBH <nicolas.lebihan@socotec.com>
2026-03-11 07:59:55 +0100

    Update project-documents.js

--- js/views/project-documents.js
+++ js/views/project-documents.js
@@ -1,6 +1,9 @@
 export function renderProjectDocuments(root) {
   root.innerHTML = `
     <h2>Documents</h2>
-    <button>Télécharger PDF</button>
+    <div class="form-row">
+      <label>PDF étude</label>
+      <input id="pdfFile" type="file" accept="application/pdf">
+    </div>
   `;
 }
