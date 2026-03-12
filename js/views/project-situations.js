96e6c12382c7f9c477c43cb14931fbf22b4f2a1a
NicoLBH <nicolas.lebihan@socotec.com>
2026-03-12 16:33:08 +0100

    Update project-situations.js

--- js/views/project-situations.js
+++ js/views/project-situations.js
@@ -2785,15 +2785,8 @@ export function renderProjectSituations(root) {
                 <option value="avis">Avis</option>
               </select>
             </label>
-            <div class="results-bar__right">
-              <div class="issues-totals mono" id="situationsHeaderCounts">—</div>
-            </div>
-            <label class="gh-filter gh-filter--inline">
-                <input id="situationsSearch" class="gh-input gh-input--sm gh-input--search" type="text" placeholder="topic / EC8 / mot-clé…" />
-                <span class="icon-search">
-                  <svg aria-hidden="true" focusable="false" class="octicon octicon-search" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" display="inline-block" overflow="visible" style="vertical-align:text-bottom"><path d="M10.68 11.74a6 6 0 0 1-7.922-8.982 6 6 0 0 1 8.982 7.922l3.04 3.04a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215ZM11.5 7a4.499 4.499 0 1 0-8.997 0A4.499 4.499 0 0 0 11.5 7Z"></path></svg>
-                </span>
-              </label>
+            <div class="issues-totals mono" id="situationsHeaderCounts">—</div>
+            
             <div class="gh-filters gh-filters--inline">
               <label class="gh-filter gh-filter--inline">
                 <span>Verdict</span>
@@ -2810,9 +2803,18 @@ export function renderProjectSituations(root) {
                   <option value="WARNING">WARNING</option>
                 </select>
               </label>
-              ${renderProjectSituationsRunbar()}
             </div>
-          </div> 
+          </div>
+
+          <div class="results-bar__right">
+            <label class="gh-filter gh-filter--inline">
+              <input id="situationsSearch" class="gh-input gh-input--sm gh-input--search" type="text" placeholder="topic / EC8 / mot-clé…" />
+              <span class="icon-search">
+                <svg aria-hidden="true" focusable="false" class="octicon octicon-search" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" display="inline-block" overflow="visible" style="vertical-align:text-bottom"><path d="M10.68 11.74a6 6 0 0 1-7.922-8.982 6 6 0 0 1 8.982 7.922l3.04 3.04a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215ZM11.5 7a4.499 4.499 0 1 0-8.997 0A4.499 4.499 0 0 0 11.5 7Z"></path></svg>
+              </span>
+            </label>
+            ${renderProjectSituationsRunbar()}
+          </div>
         </div>
       </div>
 
