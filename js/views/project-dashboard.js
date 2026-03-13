0c298c0567b9e8ce6990e01293c6222ab438c01b
NicoLBH <nicolas.lebihan@socotec.com>
2026-03-13 08:21:47 +0100

    Update project-dashboard.js

--- js/views/project-dashboard.js
+++ js/views/project-dashboard.js
@@ -1,5 +1,20 @@
+import { setProjectViewHeader, registerProjectPrimaryScrollSource } from "./project-shell-chrome.js";
+
 export function renderProjectDashboard(root) {
+  root.className = "project-shell__content";
+
+  setProjectViewHeader({
+    contextLabel: "Tableau de bord",
+    variant: "dashboard"
+  });
+
   root.innerHTML = `
-    <h2>Tableau de bord projet</h2>
+    <section class="project-simple-page">
+      <div class="project-simple-scroll" id="projectDashboardScroll">
+        <h2>Tableau de bord projet</h2>
+      </div>
+    </section>
   `;
+
+  registerProjectPrimaryScrollSource(document.getElementById("projectDashboardScroll"));
 }
