1e690aef0a376df3d432a09bc443113b13e60734
NicoLBH <nicolas.lebihan@socotec.com>
2026-03-13 08:21:05 +0100

    Update project-documents.js

--- js/views/project-documents.js
+++ js/views/project-documents.js
@@ -1,20 +1,33 @@
 import { store } from "../store.js";
+import { setProjectViewHeader, registerProjectPrimaryScrollSource } from "./project-shell-chrome.js";
 
 export function renderProjectDocuments(root) {
+  root.className = "project-shell__content";
+
+  setProjectViewHeader({
+    contextLabel: "Documents",
+    variant: "documents"
+  });
+
   const fileLabel = store.projectForm.pdfFile?.name
     ? `<div class="mono" style="margin-top:8px;">Fichier sélectionné : ${store.projectForm.pdfFile.name}</div>`
     : "";
 
   root.innerHTML = `
-    <h2>Documents</h2>
+    <section class="project-simple-page">
+      <div class="project-simple-scroll" id="projectDocumentsScroll">
+        <h2>Documents</h2>
 
-    <div class="form-row">
-      <label>PDF étude</label>
-      <input id="pdfFile" type="file" accept="application/pdf">
-      ${fileLabel}
-    </div>
+        <div class="form-row">
+          <label>PDF étude</label>
+          <input id="pdfFile" type="file" accept="application/pdf">
+          ${fileLabel}
+        </div>
+      </div>
+    </section>
   `;
 
+  registerProjectPrimaryScrollSource(document.getElementById("projectDocumentsScroll"));
   bindDocumentsEvents();
 }
 
