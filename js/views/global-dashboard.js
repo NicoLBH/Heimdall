1df9ed365e8d44ceea8c5ce771041ca03ff0a2de
NicoLBH <nicolas.lebihan@socotec.com>
2026-03-18 18:36:03 +0100

    Update global-dashboard.js

--- js/views/global-dashboard.js
+++ js/views/global-dashboard.js
@@ -1,7 +1,7 @@
 export function renderGlobalDashboard(root) {
   root.innerHTML = `
     <div class="page">
-      <h1>Dashboard</h1>
+      <h1>Accueil</h1>
       <p>Bienvenue dans Rapsobot V2</p>
     </div>
   `;
