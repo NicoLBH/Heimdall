8007b1955a4c9b47b272f155910d61f62f620c17
NicoLBH <nicolas.lebihan@socotec.com>
2026-03-21 13:24:24 +0100

    Update project-documents.js

--- js/views/project-documents.js
+++ js/views/project-documents.js
@@ -95,7 +95,7 @@ function mdToHtml(text) {
   return safe
     .replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>")
     .replace(/\*(.+?)\*/g, "<em>$1</em>")
-    .replace(/`([^`]+)`/g, "<code>$1</code>")
+    .replace(/([^]+)/g, "<code>$1</code>")
     .replace(/\n/g, "<br>");
 }
 
