4c8c1bbd12b69f4aa3fd81bceedcd836b8a8bd5d
NicoLBH <nicolas.lebihan@socotec.com>
2026-04-01 16:42:46 +0200

    Delete js directory

--- js/utils/escape-html.js
+++
@@ -1,9 +0,0 @@
-export function escapeHtml(value) {
-  return String(value ?? "").replace(/[&<>"']/g, (char) => ({
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-    '"': "&quot;",
-    "'": "&#39;"
-  }[char]));
-}
